59ec3f0c18288bdac50ac134e677f999b75b15da
Brivera12 <142284910+Brivera12@users.noreply.github.com>
2024-01-22 08:28:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const line2__bars = document.querySelector(".line2__bars_menu");
 const line3__bars = document.querySelector(".line3__bars_menu");
 const container_menu = document.querySelector(".mobile-menu");
 const menuIcono = document.querySelector('.bars__menu');
+const container_samsungLg = document.querySelector(".samsung-lg");
+const menuMarcas = document.querySelector('.marcas');
+const menuMarcas2 = document.querySelector('.marcas2');
 
 menuIcono.addEventListener('click', toggleMobileMenu);
-
+menuMarcas.addEventListener('click', toggleSamsungLg);
+menuMarcas2.addEventListener('click', toggleSamsungLg);
 function animateBars() {
     line1__bars.classList.toggle("activeline1__bars_menu");
     line2__bars.classList.toggle("activeline2__bars_menu");
@@ -16,4 +20,8 @@ function animateBars() {
 } 
  function toggleMobileMenu(){
    container_menu.classList.toggle('inactive');
+   
  }
+ function toggleSamsungLg(){
+  container_samsungLg.classList.toggle('inactive-2');
+}
